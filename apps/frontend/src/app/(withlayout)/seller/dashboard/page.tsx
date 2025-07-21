1a880ff534fa35cf8fa74621ab54a3d6a0feeac8
ronybarua-ethical18 <ronybarua.ethical18@gmail.com>
2025-07-21 20:05:16 +0600

    refactored codebase

--- apps/frontend/src/app/(withlayout)/seller/dashboard/page.tsx
+++ apps/frontend/src/app/(withlayout)/seller/dashboard/page.tsx
@@ -4,5 +4,6 @@ import SVButton from '@/components/SVButton';
 import Dashboard from '@/components/Dashboard/Dashboard';
 
 export default function DashboardPage() {
-  return <Dashboard />;
+  const test = 'test';
+                                return <Dashboard />;
 }
